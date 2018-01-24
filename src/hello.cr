f8f3a7abd0fa57daa9ff41c7ddff0b82632c4887
Spcktr <rocket155@gmail.com>
2018-01-24 23:32:05 +1100

    testing server connection

--- src/hello.cr
+++ src/hello.cr
@@ -1,7 +1,14 @@
 require "./hello/*"
 require "faker"
+require "http/server"
 
-puts Faker::Name.name
+server = HTTP::Server.new(8080) do |context|
+	context.response.content_type = "text/plain" 
+	context.response.print "Hello World! The time is #{Time.now}"
+end
+
+puts "Listening on Localhost:8080"
+server.listen
 
 # TODO: Write documentation for `Hello`
 module Hello
